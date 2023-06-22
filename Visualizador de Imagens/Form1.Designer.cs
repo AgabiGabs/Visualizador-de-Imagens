afbe197af3d5d116713e526e7d2f352f62a99b5e
Gabriel Agabi <Gabriel_agabi@hotmail.com>
2023-06-22 04:08:47 -0300

    Adicionar arquivos de projeto.

---
+++ Visualizador de Imagens/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Visualizador_de_Imagens
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ImagemListBox1 = new System.Windows.Forms.ListBox();
+            this.favoritosListBox1 = new System.Windows.Forms.ListBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.removeButton2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ImagemListBox1
+            // 
+            this.ImagemListBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ImagemListBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ImagemListBox1.FormattingEnabled = true;
+            this.ImagemListBox1.Location = new System.Drawing.Point(14, 18);
+            this.ImagemListBox1.Name = "ImagemListBox1";
+            this.ImagemListBox1.Size = new System.Drawing.Size(655, 108);
+            this.ImagemListBox1.TabIndex = 0;
+            this.ImagemListBox1.SelectedIndexChanged += new System.EventHandler(this.ImagemListBox1_SelectedIndexChanged);
+            // 
+            // favoritosListBox1
+            // 
+            this.favoritosListBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.favoritosListBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.favoritosListBox1.FormattingEnabled = true;
+            this.favoritosListBox1.Location = new System.Drawing.Point(14, 183);
+            this.favoritosListBox1.Name = "favoritosListBox1";
+            this.favoritosListBox1.Size = new System.Drawing.Size(655, 82);
+            this.favoritosListBox1.TabIndex = 1;
+            this.favoritosListBox1.SelectedIndexChanged += new System.EventHandler(this.favoritosListBox1_SelectedIndexChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(14, 333);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(654, 346);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(14, 142);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(421, 26);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Adcionar aos favoritos";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // removeButton2
+            // 
+            this.removeButton2.Location = new System.Drawing.Point(12, 286);
+            this.removeButton2.Name = "removeButton2";
+            this.removeButton2.Size = new System.Drawing.Size(423, 26);
+            this.removeButton2.TabIndex = 4;
+            this.removeButton2.Text = "Remove dos favoritos";
+            this.removeButton2.UseVisualStyleBackColor = true;
+            this.removeButton2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(690, 691);
+            this.Controls.Add(this.removeButton2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.favoritosListBox1);
+            this.Controls.Add(this.ImagemListBox1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.Text = " ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox ImagemListBox1;
+        private System.Windows.Forms.ListBox favoritosListBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button removeButton2;
+    }
+}
+
---
+++ Visualizador de Imagens/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Visualizador_de_Imagens
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] aqruivos  = Directory.GetFiles(@"C:\windows\web\Wallpaper","*.jpg",SearchOption.AllDirectories);
+            ImagemListBox1.Items.AddRange(aqruivos);
+
+            string path = obterNomeArquivoConfiguracao();
+
+            if (File.Exists(path))
+            {
+                //Ler do arquivos favoritos
+               
+                var reader = new StreamReader(path);
+                while (!reader.EndOfStream)
+                {
+                    string arquivos = reader.ReadLine();
+                    favoritosListBox1.Items.Add(arquivos);
+                }
+            }
+
+           
+
+        }
+
+        private void ImagemListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            pictureBox1.ImageLocation = ImagemListBox1.Text;
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+
+
+        }
+
+        private void favoritosListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            pictureBox1.ImageLocation = favoritosListBox1.Text;
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private string obterNomeArquivoConfiguracao()
+        {
+            var pasta = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            string arquivo = "imagensConfig.txt";
+            string path = Path.Combine(pasta, arquivo);
+            return path;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            favoritosListBox1.Items.Add(ImagemListBox1.Text);
+            gravaConfiguracao();
+
+        }
+
+        private void gravaConfiguracao()
+        {
+
+            // Gravar
+
+            string path = obterNomeArquivoConfiguracao();
+            var whriter = new StreamWriter(path);
+            foreach (string arquivos in favoritosListBox1.Items)
+            {
+                whriter.WriteLine(arquivos);
+            }
+            whriter.Close();
+        }
+    
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (favoritosListBox1.SelectedItem != null)
+            {
+                string itemSelecionado = favoritosListBox1.SelectedItem.ToString();
+                favoritosListBox1.Items.Remove(itemSelecionado);
+                gravaConfiguracao();
+            }
+        }
+    }
+}
